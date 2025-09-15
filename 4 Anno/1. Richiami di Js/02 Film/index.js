0d80a213bb71fe2a398f89c562e9c75fe6f43b77
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-09-15 11:53:41 +0200

    Update

--- 4 Anno/1. Richiami di Js/02 Film/index.js
+++ 4 Anno/1. Richiami di Js/02 Film/index.js
@@ -11,10 +11,39 @@ const films = [
     [7, "Inception", true, "18-04-2024", 5]
 ];
 
+const tBody = document.getElementsByTagName("tbody")[0]
 
-//window.addEventListener("load", function () {
-window.onload = function() {
+visualizza()
 
+function visualizza()
+{
+    tBody.innerHTML = ""
+    for(let i = 0; i < films.length;i++)
+    {
+        let tr = document.createElement("tr")
+        tBody.appendChild(tr)
 
+        for(let j = 0; j < films[i].length;j++)
+        {
+            let td = document.createElement("td")
+            tr.appendChild(td)
+            if(j == 2)
+            {
+                let chk = document.createElement("input")
+                chk.type = "checkbox"
+                chk.disabled = true
+                chk.checked = films[i][j]
+                td.appendChild(chk)
+            }
+            else if(j == 4)
+            {
+                
+            }
+            else
+            {
+                td.textContent = films[i][j]
+            }
+        }
 
+    }
 }
