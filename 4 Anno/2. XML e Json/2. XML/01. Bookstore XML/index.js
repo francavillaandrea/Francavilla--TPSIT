88e199b58b983337c1c02ddbdbaddb323a15fe8c
Andrea Francavilla <a.francavilla.3537@vallauri.edu>
2025-10-02 09:48:50 +0200

    Update tpsi-playground-francavillaandrea-1

---
+++ 4 Anno/2. XML e Json/2. XML/01. Bookstore XML/index.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const listView = document.getElementById("list-view");
+const divNbooks = listView.querySelector(".nBooks")
+const tBody = listView.querySelector("tbody");
+
+const detailsView = document.getElementById("details-view");
+detailsView.style.display="none"
+const details = detailsView.querySelector(".details")
+const pageLabel = detailsView.querySelector("span")
+
+const addView = document.getElementById("add-view");
+addView.style.display="none"
+const newBook = addView.querySelector(".details")
+
+
+
+
+
+
+
