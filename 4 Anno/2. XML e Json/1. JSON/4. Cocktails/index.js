a5eb0f77c1861806aafa4035b9a81637bb9a32b5
Andrea <sonoandre08@gmail.com>
2025-11-11 17:37:49 +0100

    Update 2. tpsi-playground-francavillaandrea-1

--- 4 Anno/2. XML e Json/1. JSON/4. Cocktails/index.js
+++ 4 Anno/2. XML e Json/1. JSON/4. Cocktails/index.js
@@ -1,22 +1,29 @@
 "use strict";
 
-let headers=["", "id", "name", "alcohlic", "main ingredient", ""]
-let headersWidth=[40, 40, 60, 70, 70, 40]
-
-
-const optAlcolico = document.getElementById("optAlcoholic")
-const optNonAlcolico = document.getElementById("optNonAlcoholic")
-const optTutti = document.getElementById("optTutti")
-const lstIngredienti=document.getElementById("lstIngredienti")
-const table = document.getElementsByTagName("table")[0];
-const details=document.getElementById("dettagli")
+// Variables
+let headers = ["", "id", "name", "alcohlic", "main ingredient", ""];
+let headersWidth = [40, 40, 60, 70, 70, 40];
+let cocktailsJson = JSON.parse(cocktails);
+let ingredientsJson = JSON.parse(ingredients);
 let tbody;
 
+// Pointers
+const optAlcohol = document.getElementById("optAlcoholic");
+const optNonAlcohol = document.getElementById("optNonAlcoholic");
+const optAll = document.getElementById("optTutti");
+const lstIngredients = document.getElementById("lstIngredienti");
+const table = document.getElementsByTagName("table")[0];
+const details = document.getElementById("dettagli");
 
+loadIngredients();
+// Functions
+function loadIngredients() {
+  lstIngredients.innerHTML = "";
+  ingredientsJson.strIngredient1.forEach(function(ingredient){
+    let option = document.createElement("option");
+    option.text = ingredient.name;
+    lstIngredients.appendChild(option);
 
-
-
-
-
-
+  });
+}
 
