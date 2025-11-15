169d39387db0fed30f8af855787363ed6cfff6e6
Andrea <sonoandre08@gmail.com>
2025-11-15 16:19:20 +0100

    Update 2. tpsi-playground-francavillaandrea-1

---
+++ 4 Anno/3. Ajax/01. RandomUser/index.js
@@ -0,0 +1,7 @@
+﻿"use strict";
+	
+let tbody = document.querySelector("tbody")
+let button = document.querySelector("button")
+button.addEventListener("click", fillTable)
+
+
---
+++ 4 Anno/3. Ajax/04. Quadri/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const head = document.querySelector('.head');
+const infoBox = document.querySelector('.info');
+const imgBox = document.querySelector('.img');
+const btns = document.querySelectorAll(button)
+const btnPrev = btns[0]
+const btnNext = btns[1]
+const wrapperAdd = document.querySelectorAll('.wrapper')[1]
+
+
+btnPrev.prop("disabled", true)
+
+
+
+
