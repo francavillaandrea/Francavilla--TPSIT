f9a9dec86042ae9c02443fa9b68207281e50248d
Andrea <sonoandre08@gmail.com>
2025-12-03 09:37:09 +0100

    Auto-update 2. tpsi-playground-francavillaandrea-1 - 2025-12-03 09:37:09

--- 4 Anno/3. jQuery/4. SpeedType/js/script.js
+++ 4 Anno/3. jQuery/4. SpeedType/js/script.js
@@ -9,3 +9,18 @@ const words = [
 
 let score;
 let time = 30;
+
+$("#startBtn").on("click", () => {
+    score = 0;
+    time = 30;
+
+    $("#score").text(score);
+    $("#time").text(time);
+
+    //Abilito typingInput
+    $("#typingInput").attr("disabled", false);
+    $("#typingInput").val();
+
+
+});
+
