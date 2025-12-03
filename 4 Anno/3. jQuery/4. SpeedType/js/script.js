32fe4a3ab705dbf61af2d2c4a162cf125f956f6e
Andrea <sonoandre08@gmail.com>
2025-12-03 09:29:48 +0100

    Update 2. tpsi-playground-francavillaandrea-1

---
+++ 4 Anno/3. jQuery/4. SpeedType/js/script.js
@@ -0,0 +1,11 @@
+const words = [
+    "computer", "javascript", "scuola", "programmare", "coding", "veloce",
+    "studio", "html", "bootstrap", "jquery", "sviluppo", "tecnologia",
+    "gioco", "digitare", "mouse", "tastiera", "logica", "funzione",
+    "schermo", "processore", "database", "internet", "algoritmo", "matrice",
+    "variabile", "condizione", "ciclo", "evento", "risorsa", "memoria",
+    "connessione", "password", "cloud", "server", "debug", "sintassi"
+];
+
+let score;
+let time = 30;
