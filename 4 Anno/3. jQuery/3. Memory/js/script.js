f0881a4862c8bcad503df22251ea164a4145cfdb
Andrea <sonoandre08@gmail.com>
2025-12-03 09:14:57 +0100

    Update 2. tpsi-playground-francavillaandrea-1

--- 4 Anno/3. jQuery/3. Memory/js/script.js
+++ 4 Anno/3. jQuery/3. Memory/js/script.js
@@ -1,68 +1,85 @@
-
-// Variabili principali
-
-let vSimboli = ["❤", "🍟", "😉", "🎈", "🥽", "🎨"];
-let carteGirate = []; // Salvo carte girate
-let bloccoClick; // blocco click multipli
+let vSymbols = ["🐱", "🐶", "♣️", "♠️", "♦️", "❤️"];
+let turnedCards = [];
+let blockClick;
+let foundCouples = 0;
+let totalCouples = vSymbols.length;
 
 $('#btnStart').click(avviaGioco);
-$('#btnMostraTutte').click(function(){
-    $('.carta').each(function(){
-        $(this).text($(this).attr("data-simbolo")).fadeIn(200);
+$('#btnMostraTutte').click(function () {
+    $('.carta').each(function () {
+        $(this).text($(this).attr("data-symbol")).fadeIn(200);
     });
 });
-$('#btnNascondiTutte').click(function(){
-    $('.carta').slideUp(500, function(){
+$('#btnNascondiTutte').click(function () {
+    $('.carta').slideUp(500, function () {
         $(this).text("?").slideDown(200);
     });
 });
 
-function avviaGioco()
-{
+function avviaGioco() {
     $('#gioco').empty(); // Rimuovo carte 
+    turnedCards = []; // Reset carte girate
+    blockClick = false; // Reset blocco
+    foundCouples = 0; // Reset coppie trovate
     $('#messaggio').text("Trova tutte le coppie!").css('color', 'green');
 
     // Creazione del mazzo di carte 
-    let mazzo = vSimboli.concat(vSimboli);
+    let mazzo = vSymbols.concat(vSymbols);
     // Mischio mazzo 
     mazzo.sort(() => Math.random() - 0.5);
     //console.log("mischio ", mazzo);
 
     // Aggiungo carte nel contenitore
-    $.each(mazzo, function(i, simbolo){
-        let carta = $("<div>").addClass("carta").text("?").attr("data-simbolo", simbolo);
+    $.each(mazzo, function (i, simbolo) {
+        let carta = $("<div>").addClass("carta").text("?").attr("data-symbol", simbolo);
         $('#gioco').append(carta);
     });
 }
 
-$('#gioco').on("click", ".carta", function(){
-    if (bloccoClick)
-        return; 
+$('#gioco').on("click", ".carta", function () {
+    if (blockClick)
+        return;
     if ($(this).text() != "?")
-        return; 
+        return;
 
-    $(this).text($(this).attr("data-simbolo"));
+    $(this).text($(this).attr("data-symbol"));
 
-    carteGirate.push($(this));
+    turnedCards.push($(this));
 
-    if(carteGirate.length == 2)
-    {
-        let [c1, c2] = carteGirate;
+    if (turnedCards.length == 2) {
+        blockClick = true; // Blocco i click
+        let [c1, c2] = turnedCards;
 
-        if(c1.attr("data-simbolo") == c2.attr("data-simbolo"))
-        {
-            c1.css("background-color", "green").animate({opacity: 0.5}, 300);
-            c2.css("background-color", "green").animate({opacity: 0.5}, 300);
-            $('#messaggio').text("Coppia trovata!");
+        if (c1.attr("data-symbol") == c2.attr("data-symbol")) {
+            // Coppia trovata!
+            foundCouples++;
+            c1.css("background-color", "green").animate({ opacity: 0.5 }, 300);
+            c2.css("background-color", "green").animate({ opacity: 0.5 }, 300);
+            $('#messaggio').text(`Coppia trovata! (${foundCouples}/${totalCouples})`).css('color', 'green');
+
+            turnedCards = [];
+            blockClick = false;
+
+            // Controllo vittoria
+            if (foundCouples == totalCouples) {
+                $('#messaggio').text("HAI VINTO!").css('color', 'gold').css('font-size', '24px');
+                blockClick = true; // Blocco ulteriori click
+            }
         }
-        else
-        {
+        else {
+            // Coppia non trovata - rigirio le carte
+            $('#messaggio').text("Sbagliato! Riprova!").css('color', 'red');
+            c1.css("background-color", "red").animate(300);
+            c2.css("background-color", "red").animate(300);
 
+            setTimeout(function () {
+                c1.text("?").css("background-color", "lightgray");
+                c2.text("?").css("background-color", "lightgray");
+                turnedCards = [];
+                blockClick = false;
+            }, 800);
         }
     }
 
 });
-
-
-
 $('#btnStart').trigger("click");
