996138ea50b946dd0424c7e6535ab24c56d45428
Andrea <sonoandre08@gmail.com>
2025-12-14 15:31:27 +0100

    Update 2. tpsi-playground-francavillaandrea-1

---
+++ 4 Anno/4. Ajax/01. RandomUser/index.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+	
+let tbody = document.querySelector("tbody")
+let button = document.querySelector("button")
+button.addEventListener("click", fillTable)
+
+function fillTable(){
+    let params = {
+        results:20,
+        gender:"male"
+    }
+                                // metodo, risorsa, parametri
+    let promise = ajax.sendRequest("GET", "/api", params)
+    promise.catch(ajax.errore)
+    promise.then(function(httpResponse){
+       console.log(httpResponse)
+       let people = httpResponse.data;
+       tbody.innerHTML=""
+       for (let person of people.results){
+           let tr = document.createElement("tr") 
+           tbody.appendChild(tr)
+
+           let td = document.createElement("td") 
+           td.textContent = person.name.first +  " " + person.name.last
+           tr.appendChild(td)
+       }
+
+    })
+}
+
+
