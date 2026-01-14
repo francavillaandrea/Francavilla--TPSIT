fdb00482c80488c1cda7e5b4d3eedb4bf902543a
unknown <a.francavilla.3537@vallauri.net>
2026-01-14 10:39:18 +0100

    Quadri

--- 4 Anno/4. Ajax/04. Quadri/index.js
+++ 4 Anno/4. Ajax/04. Quadri/index.js
@@ -3,13 +3,37 @@
 const head = document.querySelector('.head');
 const infoBox = document.querySelector('.info');
 const imgBox = document.querySelector('.img');
-const btns = document.querySelectorAll(button)
+const btns = document.querySelectorAll("button")
 const btnPrev = btns[0]
 const btnNext = btns[1]
 const wrapperAdd = document.querySelectorAll('.wrapper')[1]
 
+btnPrev.disabled = true;
 
-btnPrev.prop("disabled", true)
+
+getArtists();
+//<label> <input type='radio'> artistname </label> 
+
+function getArtists() {
+    let promise = ajax.sendRequest("GET", "/artisti")
+    promise.catch(function (httpResponse) {
+        let label, input, opt;
+        let artisti = httpResponse.data;
+        artisti.forEach(artista => {
+            
+            label = document.createElement("label");
+            label.textContent = artista.name;
+            head.append(label);
+
+            input = document.createElement("input");
+            input.type = "radio";
+            input.id = artista.id;
+            label.append(input);
+
+            
+        });
+    });
+}
 
 
 
