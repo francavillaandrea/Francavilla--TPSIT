7ad96d01de3d1df97fd9993ac35f4dd3216f2f0c
unknown <a.francavilla.3537@vallauri.net>
2026-01-26 11:53:39 +0100

    Sdobberman

--- 4 Anno/4. Ajax/04. Quadri/index.js
+++ 4 Anno/4. Ajax/04. Quadri/index.js
@@ -1,153 +1,161 @@
 "use strict"
+
 const head = document.querySelector('.head');
 const infoBox = document.querySelector('.info');
 const imgBox = document.querySelector('.img');
-const btns = document.querySelectorAll('button')
+const btns = document.querySelectorAll("button")
 const btnPrev = btns[0]
 const btnNext = btns[1]
 const wrapperAdd = document.querySelectorAll('.wrapper')[1]
-let quadri = []
-let indiceQuadro = 0
-let like
+let quadri;
+let indiceQuadro;
 
-btnPrev.disabled = true
-getArtists()
+btnPrev.disabled = true;
+
+getArtists();
 
 function getArtists() {
-    let promise = ajax.sendRequest("GET", "/artisti")
-    promise.catch(ajax.errore)
+    let promise = ajax.sendRequest("GET", "/artisti");
+    promise.catch(ajax.errore);
     promise.then(function (httpResponse) {
-        console.log(httpResponse.data)
-        let artisti = httpResponse.data
+        let artisti = httpResponse.data;
+        let label, input, opt;
 
         artisti.forEach(function (artista) {
-            const label = document.createElement("label")
-            head.appendChild(label)
+            //<!-- <label> <input type='radio'> artistname </label> -->
+            label = document.createElement("label");
+            head.appendChild(label);
 
-            const input = document.createElement("input")
-            input.type = "radio"
-            // Dataset non accetta oggetti richede espressamente delle stringhe. Dunque conviene convertire il json a stringa
-            input.dataset.artista = JSON.stringify(artista)
-            input.id = artista.id
-            input.name = "artisti"
-            input.addEventListener("change", function () {
-                getQuadri(JSON.parse(input.dataset.artista))
-            })
-            label.appendChild(input)
-
-            //label.textContent = artista.name
-            // textContent sovrascrive l' HTML, al suo posto conviene utlizzare il metodo createTextNode{
-            //label.append(artista.name) visto che append accetta stringhe
-
-            label.append(document.createTextNode(artista.name))
+            input = document.createElement("input");
+            input.type = "radio";
+            input.id = artista.id;
+            //Dataset non accetta oggetti richiede espressamente delle stringhe
+            //dunque conviene serializzare l'oggetto
+            input.dataset.artista = JSON.stringify(artista);
+            input.name = "artisti";
+            input.addEventListener("click", () => {
+                getQuadri(artista);
+            });
+            label.appendChild(input);
+            //Text content sovrascrive l'HTML,
+            //label.textContent = artista.name;
+            //al suo posto conviene utilizzare il metodo createTextNode
+            label.appendChild(document.createTextNode(artista.name));
+            //Oppure label.append(stringa)
         });
 
-        let nArtista = random(0, artisti.length)
+        let nArtista = generaNumero(0, artisti.length);
+        let id = artisti[nArtista].id;
+        //Notare che le "" sono fondamentali in quanto querySelector è una
+        //combinazione di js e css. Passando i parametri senza "" css potrebbe
+        //non riconoscere il valore dunque è obbligatorio l'uso delle virgolette
+        //così da evitare comportamenti inaspettati
+        head.querySelector(`input[type="radio"][id="${id}"]`).checked = true;
 
-        let id = artisti[nArtista].id
-        // Notare che le "" sono fondamentali in quanto querySelector è una combinazione di js e css. Passando i parametri senza "" il css potrebbe non riconoscere il valore dunque è obbligatorio l'uso delle virgolette cosi da evitare comportamenti inaspettati
-        head.querySelector(`input[type = "radio"][id = "${id}"]`).checked = true
-
-        getQuadri(artisti[nArtista])
-    })
+        getQuadri(artisti[nArtista]);
+    });
 }
 
 function getQuadri(artista) {
-    let promise = ajax.sendRequest("GET", "/quadri", `{artist: ${artista.id}}`)
-    promise.catch(ajax.errore)
+    let promise = ajax.sendRequest("GET", "/quadri", { artist: `${artista.id}` });
+    promise.catch(ajax.errore);
     promise.then(function (httpResponse) {
-        quadri = httpResponse.data
-        console.log(quadri)
+        quadri = httpResponse.data;
 
-        indiceQuadro = 0
-        btnPrev.disabled = true
+        indiceQuadro = 0;
+        btnPrev.disabled = true;
         if (quadri.length > 1) {
-            btnNext.disabled = false
+            btnNext.disabled = false;
         }
         else {
-            btnNext.disabled = true
+            btnNext.disabled = true;
         }
-        visualizzaQuadro(indiceQuadro)
-    })
+        visualizzaQuadro(quadri[indiceQuadro]);
+    });
 }
 
-// Il parametro della funzione sottostante non è l'id del quadro, ma è l'indice del quadro all'interno del vettore quadri
-function visualizzaQuadro(indice) {
-    let artista = JSON.parse(head.querySelector(`input[type="radio"]:checked`).dataset.artista)
+// Il parametro della funzione sottostante non è l'id del quadro,
+// ma è l'indice del quadro nel vettore quadri
+function visualizzaQuadro(quadro) {
+    let artista = JSON.parse(head.querySelector(`input[type="radio"]:checked`).dataset.artista);
+    //Notare che facendo l'evento nel HTML non ci sarà un this e che se gli si passa un'oggetto
+    //esso va serializzato.
+    infoBox.innerHTML =
+        `
+        <div>ID=<b>${quadro.id}</b></div>
+        <div>TITOLO=<b>${quadro.title}</b></div>
+        <div>GENERE=<b>${artista.gender}</b></div>
+        <div>
+            LIKE=<b>${quadro.nLike}</b>
+            <img src="./img/like.jpg" class="like" onclick='addLike(${JSON.stringify(quadro)})'>
+        </div>
+    `;
 
-    let quadro = quadri[indice]
+    infoBox.querySelector("img"), addEventListener("click", () => {
+        addLike(quadro);
+    });
 
-    infoBox.innerHTML = ""
-    imgBox.innerHTML = ""
-
-    let div = document.createElement("div")
-    infoBox.appendChild(div)
-    let b = document.createElement("b")
-    div.appendChild(b)
-    b.textContent = "ID = " + quadro.id
-
-    div = document.createElement("div")
-    infoBox.appendChild(div)
-    b = document.createElement("b")
-    div.appendChild(b)
-    b.textContent = "Titolo = " + quadro.title
-
-    div = document.createElement("div")
-    infoBox.appendChild(div)
-    b = document.createElement("b")
-    div.appendChild(b)
-    b.textContent = "Genere = " + artista.gender
-
-    div = document.createElement("div")
-    infoBox.appendChild(div)
-    b = document.createElement("b")
-    div.appendChild(b)
-    b.textContent = "Like = " + quadro.nLike
-    let btnLike = document.createElement("button")
-    div.appendChild(btnLike)
-    btnLike.classList.add("like")
-    btnLike.addEventListener("click", function () {
-        quadro.nLike++
-
-        const promise = ajax.sendRequest("PATCH", `/quadri/${quadro.id}`, { nLike: quadro.nLike });
-        promise.catch(ajax.errore);
-        promise.then(function (httpResponse) {
-            getQuadri(artista)
-        })
-    })
-
+    //Se si usa innerHTML lo style nel tag deve essere in formato CSS non JS
+    //NOTA: Se hai un'immagine in formato base64 non si deve mettere il path ma solo
+    //il nome del file con estensione
+    let img = quadro.img.startsWith("data:image/") ? quadro.img : `./img/${quadro.img}`;
     imgBox.innerHTML =
         `
-    <img src="./img/${quadro.img}" title="immagine quadro" alt="Immagine non disponibile">
-    `
+        <img src="${img}" style="max-height:120px" title="immagine quadro" alt="immagine non disponibile"></img>
+    `;
 }
 
-btnNext.addEventListener("click", function () {
-    indiceQuadro++
-    visualizzaQuadro(indiceQuadro)
+btnNext.addEventListener("click", () => {
+    indiceQuadro++;
+    visualizzaQuadro(quadri[indiceQuadro]);
 
     if (indiceQuadro >= quadri.length - 1) {
-        btnNext.disabled = true
+        btnNext.disabled = true;
     }
-    if (indiceQuadro > 0) {
-        btnPrev.disabled = false
-    }
+    btnPrev.disabled = false;
+});
 
-})
-
-btnPrev.addEventListener("click", function () {
+btnPrev.addEventListener("click", () => {
     indiceQuadro--;
-    let artista = JSON.parse(head.querySelector(`input[type="radio"]:checked`).dataset.artista)
-    visualizzaQuadro(indiceQuadro)
+    visualizzaQuadro(quadri[indiceQuadro]);
 
-    if (indiceQuadro > 0) {
-        btnPrev.disabled = false
+    if (indiceQuadro == 0) {
+        btnPrev.disabled = true;
     }
-    else {
-        btnPrev.disabled = true
-    }
-})
+    btnNext.disabled = false;
+});
 
-function random(min, max) {
-    return Math.floor((max - min) * Math.random() + min)
+async function addLike(quadro) {
+    let likeIncrementati = quadro.nLike + 1;
+    //Await blocca il codice finché non viene risolta la promise e restituisce il valore che
+    //verrebbe normalmente inniettato al then
+    const httpResponse = await ajax.sendRequest("PATCH", `/quadri/${quadro.id}`, { nLike: likeIncrementati })
+        .catch(ajax.errore);
+
+    //Controllo che la risposta non sia undefined, quindi solo se la promise ha avuto successo
+    if (httpResponse) {
+        //Abbiamo 3 tipi di richieste get possibili in questo caso
+        //1. Direttamente in urlencoded
+        const httpResponse2 = await ajax.sendRequest("GET", `/quadri?id=${quadro.id}`).catch(ajax.errore);
+        if (httpResponse2) {
+            let quadroAggiornato = httpResponse2.data[0];
+
+            quadri[indiceQuadro] = quadroAggiornato;
+            visualizzaQuadro(quadri[indiceQuadro]);
+        }
+        //2. Gli passiamo l'inizio del link e un vettore associativo che però funziona solo nel nostro
+        //caso visto che converte l'oggetto in urlencoded
+        //const httpResponse2 = await ajax.sendRequest("GET","/quadri",{id: quadro.id});
+        //3. Questa è di nuovo standard e funziona solamente con un'id.
+        //const httpResponse2 = await ajax.sendRequest("GET",`/quadri/${quadro.id}`);
+
+        //NOTA: Le modalità 1 e 2 restituisce un vettore perché sono generiche
+        //mentre la terza modalità restituisce un solo JSON e non un vettore
+    }
+
 }
+
+function generaNumero(min, max) {
+    return Math.floor((max - min) * Math.random()) + min;
+}
+
