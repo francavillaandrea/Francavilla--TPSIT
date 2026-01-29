8180b9697ac89c546cfaa1b896059e36b5034555
unknown <a.francavilla.3537@vallauri.net>
2026-01-29 10:21:44 +0100

    Bebevio

--- 4 Anno/4. Ajax/05. Concerti/index.js
+++ 4 Anno/4. Ajax/05. Concerti/index.js
@@ -1,106 +1,142 @@
-"use strict";
+"use strict"
 
-const tbody = document.querySelector("tbody");
-
-// Prendo correttamente i due dropdown
-const dropdownMenuCity = lstCitta.querySelector(".dropdown-menu");
-const dropdownMenuGenre = lstGeneri.querySelector(".dropdown-menu");
+const tbody = document.querySelector("tbody")
+const dropdownMenuCitta = lstCitta.querySelector(".dropdown-menu");
+const dropdownMenuGenere = lstGeneri.querySelector(".dropdown-menu");
 
 divDettagli.style.display = "none";
 
 getCities();
-getGenres();
+getGenders();
 
-/* ------------------ CITTÀ ------------------ */
+//#region gestione dropdown
 async function getCities() {
     let httpResponse = await ajax.sendRequest("GET", "/citta").catch(ajax.errore);
     if (httpResponse) {
         let cities = httpResponse.data;
 
-        dropdownMenuCity.appendChild(createDropdownItem("Tutte"));
+        dropdownMenuCitta.appendChild(createDropdownItem("Tutte"));
         for (let citta of cities) {
-            dropdownMenuCity.appendChild(createDropdownItem(citta.citta));
+            dropdownMenuCitta.appendChild(createDropdownItem(citta.citta));
         }
     }
 }
-
-dropdownMenuCity.addEventListener("click", function (event) {
-    if (!event.target.classList.contains("dropdown-item")) return;
-
-    this.previousElementSibling.textContent = event.target.textContent;
-    getConcerts();
-});
-
-/* ------------------ GENERI ------------------ */
-async function getGenres() {
+async function getGenders() {
     let httpResponse = await ajax.sendRequest("GET", "/generi").catch(ajax.errore);
     if (httpResponse) {
-        let genres = httpResponse.data;
+        let generi = httpResponse.data;
 
-        dropdownMenuGenre.appendChild(createDropdownItem("Tutte"));
-        for (let genre of genres) {
-            dropdownMenuGenre.appendChild(createDropdownItem(genre.genere));
+        dropdownMenuGenere.appendChild(createDropdownItem("Tutti"));
+        for (let genere of generi) {
+            dropdownMenuGenere.appendChild(createDropdownItem(genere.genere));
         }
     }
 }
 
-dropdownMenuGenre.addEventListener("click", function (event) {
-    if (!event.target.classList.contains("dropdown-item")) return;
-
+//Metto l'evento al padre e gli dico di passarmi i valori del figlio che l'ha scatenato
+//NOTA: va pefforza passato l'evento come parametro all'interno di function()
+function clickDropdownMenuElement(event) {
     this.previousElementSibling.textContent = event.target.textContent;
     getConcerts();
-});
+}
+dropdownMenuCitta.addEventListener("click", clickDropdownMenuElement);
+dropdownMenuGenere.addEventListener("click", clickDropdownMenuElement);
 
-/* ------------------ CONCERTI ------------------ */
+function createDropdownItem(text) {
+    let aTag = document.createElement("a");
+    aTag.classList.add("dropdown-item");
+    aTag.href = "#";
+    aTag.textContent = text;
+
+    return aTag;
+}
+//#endregion
+
+// #region creazione tabella
 function getConcerts() {
+    tbody.innerHTML = "";
+
     let city = lstCitta.querySelector("button").textContent;
     let genre = lstGeneri.querySelector("button").textContent;
 
     let filter = {};
-
     if (city != "Tutte" && !city.includes("Città")) {
-        filter["sede.città"] = city;
+        filter["sede.citta"] = city;
     }
-
     if (genre != "Tutti" && !genre.includes("Generi")) {
         filter["genere"] = genre;
     }
 
     const promise = ajax.sendRequest("GET", "/concerti", filter);
     promise.catch(ajax.errore);
-    promise.then(() => {
-        let concerts = httpResponse;
+    promise.then(function (httpResponse) {
+        let concerts = httpResponse.data;
 
-        concerts.forEach(concert => {
-            let trTag = document.createElement("tr");
-            tbody.append(trTag);
-            trTag.appendChild(creaCella(concert.id));
-            trTag.appendChild(creaCella(concert.cantante));
-            trTag.appendChild(creaCella(concert.data));
-            trTag.appendChild(creaCella(concert.genere));
-            trTag.appendChild(creaCella(concert.sede.citta));
-            trTag.appendChild(creaCella(concert.sede.struttura));
+        for (let concert of concerts) {
+            let tr = document.createElement("tr");
+            tbody.appendChild(tr);
 
-            let tdTag = creaCella();
-            let btn = document.createElement("button");
-            tdTag.append(btn);
-            btn.textContent = "Dettagli";
-            btn.classList.add("btn", "btn-info", "btn-xs ");
-            trTag.append(tdTag);
+            tr.appendChild(creaElementoTabella(concert.id));
+            tr.appendChild(creaElementoTabella(concert.cantante));
+            tr.appendChild(creaElementoTabella(concert.data));
+            tr.appendChild(creaElementoTabella(concert.genere));
+            tr.appendChild(creaElementoTabella(concert.sede.citta));
+            tr.appendChild(creaElementoTabella(concert.sede.struttura));
 
-        });
+            tr.appendChild(creaElementoTabella("Prenota", ["btn", "btn-success", "btn-xs"]));
+            tr.appendChild(creaElementoTabella("Dettagli", ["btn", "btn-info", "btn-xs"]));
+        }
+    });
+}
+
+function creaElementoTabella(text, classes) {
+    let td = document.createElement("td");
+    //Visto che agli elementi della tabella non vanno aggiunte classi a meno che non
+    //siano dei bottoni possiamo controllare se ci passano delle classi
+    if (!classes) {
+        td.textContent = text;
+    }
+    else {
+        let btn = document.createElement("button");
+        td.appendChild(btn);
+        btn.textContent = text;
+
+        if (text == "Dettagli") {
+            btn.addEventListener("click", showDetails)
+        }
+        else {
+            btn.addEventListener("click", () => {
+                bookConcert(concert);
+            })
+        }
+
+        for (let auxClass of classes) {
+            btn.classList.add(auxClass);
+        }
+    }
+
+    return td;
+}
+
+function showDetails() {
+    let concertId = this.parentElement.parentElement.firstElementChild.textContent;
+    const promise = ajax.sendRequest("GET", `/concerti/${concertId}`);
+    promise.catch(ajax.errore);
+    promise.then(httpResponse => {
+        let data = httpResponse;
+        caricaDettagli([concert.data, concert.sede.città,]);
 
     })
 }
-/*  --------------- UTILS ------------------ */
-function createDropdownItem(text) {
-    let aTag = document.createElement("a");
-    aTag.classList.add("dropdown-item");
-    aTag.href = "#";
-    aTag.textContent = text;
-    return aTag;
-}
-
-function creaCella(concert) {
+function bookConcert(concert) {
 
 }
+
+function caricaDettagli(vDetails) {
+    vDetails.forEach((element,i) => {
+        let span = divDettagli.querySelector(`div:nth-of-type(${i+1}) span:nth-of-type(2)`)
+        span.textContent = element;
+        console.log(element)
+    });
+}
+// #endregion
