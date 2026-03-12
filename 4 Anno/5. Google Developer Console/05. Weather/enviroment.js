70ecfe917f1c51daa4261b2e320c9326a318023b
unknown <a.francavilla.3537@vallauri.net>
2026-03-12 10:23:56 +0100

    Update

--- 4 Anno/5. Google Developer Console/05. Weather/enviroment.js
+++
@@ -1,2 +0,0 @@
-"use strict"
-const API_KEY_WEATHER = "c5de2accda48a5c9a19b95146d19ce0d"
--- 4 Anno/5. Google Developer Console/05. Weather/script.js
+++ 4 Anno/5. Google Developer Console/05. Weather/script.js
@@ -3,28 +3,38 @@
 const address = "Rome, Italy";
 const mapStyle = myMapLibre.openMapsStyle;
 const zoom = 5.5;
-const API_KEY_WEATHER = "c5de2accda48a5c9a19b95146d19ce0d"
+const API_KEY_WEATHER = "76057cb5fa5b3fea9eb4824877c0eed2"
 const jsonServer = "http://localhost:3000/citta"
-const weatherServer = "http://api.openweathermap.org/data/2.5/weather" + API_KEY_WEATHER;
+const weatherServer = "http://api.openweathermap.org/data/2.5/weather?units=metric&lang=it&APPID=" + API_KEY_WEATHER;
 
-loadPage()
+avvioPagina();
 
-async function loadPage() {
-    await loadMap();
-    const cities = await ajax.sendRequest("GET", jsonServer).catch(console.error)
+//Le funzioni che richiamano funzioni asincrone DEVONO essere anch'esse asincrone
+//Se una funziona asincrona restituisce un valore esso verrà iniettato all'interno del then della promise
+async function avvioPagina() {
+    await caricaMappa();
 
-    if (cities) {
-        for (let city of cities.data) {
-            let dataWeather = await ajax.sendRequest("GET", weatherServer, { "q": cities[0].nome }).catch(console.error)
+    const httpResponse = await ajax.sendRequest("GET", jsonServer).catch(console.error);
+
+    if (httpResponse) {
+        let cities = httpResponse.data;
+        for (let city of cities) {
+            const httpResponseMeteo = await ajax.sendRequest("GET", weatherServer, { "q": cities[0].nome + ",it" })
+                .catch(console.error);
+
+            if (httpResponseMeteo) {
+                let meteoInfo = httpResponseMeteo.data;
+            }
         }
     }
+
 }
 
-async function loadMap() {
+async function caricaMappa() {
     const gpsAddress = await myMapLibre.geocode(address);
 
     if (gpsAddress) {
-        await myMapLibre.drawMap(mapStyle, mapContainer, gpsAddress, zoom)
+        await myMapLibre.drawMap(mapStyle, mapContainer, gpsAddress, zoom);
     }
-
 }
+
