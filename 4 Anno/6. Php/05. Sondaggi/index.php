9cd7ccc7ca4027f0a535c6d2233f0160129a7841
Francavillaandrea <a.francavilla.3537@vallauri.net>
2026-03-30 11:53:45 +0200

    Update/Php

--- 4 Anno/6. Php/05. Sondaggi/index.php
+++ 4 Anno/6. Php/05. Sondaggi/index.php
@@ -8,13 +8,24 @@
     </head>
 
     <body>
+        <?php
+        require("ClsMySQL.php");
+        //Creiamo la connesione al DB
+        $dbClient = new ClsMySQL();
+        //sql è case sensitive sui campi ma non sulle istruzioni delle query (select, from, like ecc)
+        $sql = "SELECT id,titolo FROM sondaggi";
+        //richiama il metodo che manda la query al db
+        $data = $dbClient->select($sql);
+        ?>
         <div id="wrapper">
             <h1 align="center"> Seleziona il sondaggio a cui desideri partecipare</h1>
             <hr>
             <h3>Sondaggi disponibili:</h3>
             <form>
                 <select id='lstSondaggi' name='lstSondaggi'>
-
+                    <?php
+                    //echo ("<option>Ok</option>");
+                    ?>
                 </select>
                 <button type="button" id="btnInvia"> invia </button>
             </form>
